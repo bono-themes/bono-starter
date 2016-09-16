ee37d0edb8a7525cffef08130a5f03e203f2b0dc
David Yang <daworld.ny@gmail.com>
2016-09-16 23:03:31 +0500

    Basic configuration file

--- theme.config.php
+++ theme.config.php
@@ -1,5 +1,28 @@
 <?php
 
 return array(
+    'meta' => array(
+        'name' => 'Starter',
+        'description' => 'Starter theme Bono CMS',
+        'version' => '1.1',
+        'author' => 'NGS Labs'
+    ),
 
+	// Those plugins are defined in /config/view.php
+	'plugins' => array(
+		'jquery', 
+		'bootstrap.blue', 
+		'bootstrap.core',
+        'font-awesome'
+	),
+	
+	// Style sheets and scripts for this theme to load
+	'theme' => array(
+		'stylesheets' => array(
+			'/css/styles.css',
+		),
+		'scripts' => array(
+			'/js/app.js'
+		)
+	)
 );
